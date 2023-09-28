d873374d0ce0bbb0516c38c8e9c0e912fd187226
Rionne Angelo Ugale <52653430+aeolus28@users.noreply.github.com>
2023-09-28 18:33:33 +0800

    Update streamlit_app.py
    changed header title
    inserted avocado and strawberry filter to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-streamlit.title('My Parents New Healthy Diner')  
+streamlit.title('My Mom\'s New Healthy Diner')  
 
 streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
@@ -17,7 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
