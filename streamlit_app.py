2a7358e9d812ef2e0d4b08e6b29c8bea41c350a1
Rionne Angelo Ugale <52653430+aeolus28@users.noreply.github.com>
2023-10-01 18:37:08 +0800

    Update streamlit_app.py
    added snowlflake details after connecting each one to the other

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 # streamlit.text(fruityvice_response.json()) 
