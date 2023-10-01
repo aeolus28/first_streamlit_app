e7e882fdadff60acea3f0db2bd0cdbc3b1c33e7d
Rionne Angelo Ugale <52653430+aeolus28@users.noreply.github.com>
2023-10-01 19:36:41 +0800

    Update streamlit_app.py
    fetchtwo to fetchall

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,9 @@ fruityvice_response = requests. get("https://fruityvice.com/api/fruit/" + fruit_
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchtwo()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 
